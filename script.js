0e699ab7d87fe1fb27e46a2454f14131844991dc
Diaa-E <blarb21@yahoo.com>
2022-09-10 23:24:09 +0200

    Add password match validation

--- script.js
+++ script.js
@@ -1,16 +1,24 @@
 "use strict";
 
-//take only required fields
-const pwdFields = document.querySelectorAll("input[type='password']")
+//fetch password fields
+const pwdFields = document.querySelectorAll("input[type='password']");
+const msg = "Passwords are not matching";
 
 pwdFields.forEach(field => {
 
     //input triggers when anything is put into a field unlike change
     field.addEventListener("input", (e) =>{
         
-        if (pwdFields[0].innerHTML === pwdFields[1])
+        if (pwdFields[0].value !== pwdFields[1].value)
         {
-
+            pwdFields[0].setCustomValidity(msg);
+            pwdFields[1].setCustomValidity(msg);
+        }
+        else
+        {
+            //marks as valid
+            pwdFields[0].setCustomValidity("");
+            pwdFields[1].setCustomValidity("");
         }
     })
 })
